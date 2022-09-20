50af4fdc3b4ab002750da32c0e9c6ea63ccfeb2a
kw@test.com <kw2test.com>
2022-09-20 11:16:58 -0500

    saving changes/testing gitignore

---
+++ WordFrequency.py
@@ -0,0 +1,22 @@
+#read the file into the program 
+
+infile = open('sometext.txt', 'r')
+textfile = infile.read()
+
+#create a dictionary 
+
+
+
+
+#create for loop 
+
+
+
+
+
+
+
+
+
+#close the file
+infile.close()
