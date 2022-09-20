0673ba97f7378a14be8c1a2cc533d7dd964c42cb
kw@test.com <kw2test.com>
2022-09-20 12:11:53 -0500

    testing

--- WordFrequency.py
+++ WordFrequency.py
@@ -1,16 +1,18 @@
 #read the file into the program 
 
 infile = open('sometext.txt', 'r')
-textfile = infile.read()
+#textfile = infile.read()
 
 #create a dictionary 
 
-
+mydictionary = {}
 
 
 #create for loop 
 
-
+for line in infile: 
+    for word in line.split():
+        print(word)
 
 
 
